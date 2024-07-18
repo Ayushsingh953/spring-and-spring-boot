d6e80bdb39e56f95c39481dec5ae08268d968c2c
ayushsingh953 <ayushsingh35323@gmail.com>
2024-07-18 14:21:54 +0530

    new class

--- exercises/first-program/src/main/java/com/ayush953/first_program/AppGamingBasicJava.java
+++ exercises/first-program/src/main/java/com/ayush953/first_program/AppGamingBasicJava.java
@@ -4,8 +4,9 @@ import com.ayush953.first_program.game.*;
 
 public class AppGamingBasicJava {
     public static void main(String[] args) {
-        Mario game = new Mario();
+//        Mario game = new Mario();
 //        SuperContra game = new SuperContra();
+        Pacman game = new Pacman();
         GameRunner gameRunner = new GameRunner(game);
         gameRunner.run();
     }
---
+++ exercises/first-program/src/main/java/com/ayush953/first_program/game/Pacman.java
@@ -0,0 +1,16 @@
+package com.ayush953.first_program.game;
+
+public class Pacman implements GamingConsole{
+    public void up() {
+        System.out.println("Pacman up");
+    }
+    public void down() {
+        System.out.println("Pacman down");
+    }
+    public void left() {
+        System.out.println("Pacman left");
+    }
+    public void right() {
+        System.out.println("Pacman right");
+    }
+}
