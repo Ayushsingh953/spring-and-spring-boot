d60a8f16e14f9a3d72fbe0fcae419b8595d9c144
ayushsingh953 <ayushsingh35323@gmail.com>
2024-07-18 14:07:57 +0530

    initialised new spring project

---
+++ exercises/first-program/src/test/java/com/ayush953/first_program/FirstProgramApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ayush953.first_program;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstProgramApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
