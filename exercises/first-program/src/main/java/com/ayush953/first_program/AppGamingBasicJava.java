f69c251bb0de3a2bd73576cf59a8b0497342da2e
ayushsingh953 <ayushsingh35323@gmail.com>
2024-07-18 14:27:57 +0530

    added comment

--- exercises/first-program/src/main/java/com/ayush953/first_program/AppGamingBasicJava.java
+++ exercises/first-program/src/main/java/com/ayush953/first_program/AppGamingBasicJava.java
@@ -6,8 +6,8 @@ public class AppGamingBasicJava {
     public static void main(String[] args) {
 //        Mario game = new Mario();
 //        SuperContra game = new SuperContra();
-        Pacman game = new Pacman();
-        GameRunner gameRunner = new GameRunner(game);
+        Pacman game = new Pacman(); // Object creation
+        GameRunner gameRunner = new GameRunner(game); // Object creation + wiring of dependency
         gameRunner.run();
     }
 }
