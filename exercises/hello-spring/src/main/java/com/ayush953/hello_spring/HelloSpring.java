45eacc8843139faf7c5496cff4ca39d4b3b80c1f
ayushsingh953 <ayushsingh35323@gmail.com>
2024-07-18 14:42:57 +0530

    created a bean in spring context

--- exercises/hello-spring/src/main/java/com/ayush953/hello_spring/HelloSpring.java
+++ exercises/hello-spring/src/main/java/com/ayush953/hello_spring/HelloSpring.java
@@ -6,7 +6,12 @@ public class HelloSpring {
     public static void main(String[] args) {
         // Configure things that we want spring to manage : @Configuration class
         // Configured in HelloSpringConfiguration class
+        // Created bean named name : @Bean
+
         // Launch Spring context
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(HelloSpringConfiguration.class);
+
+        // Retrieve name context from spring
+        System.out.println(context.getBean("name"));
     }
 }
--- exercises/hello-spring/src/main/java/com/ayush953/hello_spring/HelloSpringConfiguration.java
+++ exercises/hello-spring/src/main/java/com/ayush953/hello_spring/HelloSpringConfiguration.java
@@ -1,7 +1,12 @@
 package com.ayush953.hello_spring;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class HelloSpringConfiguration {
+    @Bean
+    public String name(){
+        return "Ayush";
+    }
 }
