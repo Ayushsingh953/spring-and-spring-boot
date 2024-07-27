c441e7b87aea052ddfc7538a7caf08b90a6423fe
ayushsingh953 <ayushsingh35323@gmail.com>
2024-07-27 15:29:16 +0530

    JSP - Java Server Paages

--- projects/first-web-app/src/main/java/com/ayush953/first_web_app/hello/HelloController.java
+++ projects/first-web-app/src/main/java/com/ayush953/first_web_app/hello/HelloController.java
@@ -9,15 +9,20 @@ public class HelloController {
 
     @RequestMapping("/hello")
     @ResponseBody
-    public String Hello(){
+    public String hello(){
         return "Hello World";
     }
 
     @RequestMapping("/hello-html")
     @ResponseBody
-    public String HelloHtml(){
+    public String helloHtml(){
         StringBuffer sb = new StringBuffer();
         sb.append("<h1>Hello World</h1>");
         return sb.toString();
+    }
+
+    @RequestMapping("/hello-jsp")
+    public String helloJsp(){
+        return "sayHello";
     }
 }
