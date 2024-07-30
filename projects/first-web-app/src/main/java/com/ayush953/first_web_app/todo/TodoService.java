08ea6228c45cb221c6caa0b92bda855b4b4824fd
ayushsingh953 <ayushsingh35323@gmail.com>
2024-07-30 23:38:19 +0530

    returning todos

--- projects/first-web-app/src/main/java/com/ayush953/first_web_app/todo/TodoService.java
+++ projects/first-web-app/src/main/java/com/ayush953/first_web_app/todo/TodoService.java
@@ -4,11 +4,15 @@ import java.time.LocalDate;
 import java.util.List;
 
 public class TodoService {
-    static List<Todo> todos;
+    private static List<Todo> todos;
 
     static {
         todos.add(new Todo(1,"Ayush","Learn Apache Kafka", LocalDate.now().plusYears(1),false));
         todos.add(new Todo(2,"Ayush","Learn Docker", LocalDate.now().plusYears(1),false));
         todos.add(new Todo(3,"Ayush","Learn Spring Boot", LocalDate.now().plusYears(1),false));
+    }
+
+    public List<Todo> getTodosByName(String name) {
+        return todos;
     }
 }
