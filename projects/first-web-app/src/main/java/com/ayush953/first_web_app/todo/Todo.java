4763268100505547f14eae089ca2607f9d4dd962
ayushsingh953 <ayushsingh35323@gmail.com>
2024-07-30 23:36:45 +0530

    todo class and todo service

---
+++ projects/first-web-app/src/main/java/com/ayush953/first_web_app/todo/Todo.java
@@ -0,0 +1,54 @@
+package com.ayush953.first_web_app.todo;
+
+import java.time.LocalDate;
+
+public class Todo {
+    private int id;
+    private String username;
+    private String description;
+    private LocalDate targetDate;
+    private boolean completed;
+
+    public Todo(int id, String username, String description, LocalDate targetDate, boolean completed) {
+        this.id = id;
+        this.username = username;
+        this.description = description;
+        this.targetDate = targetDate;
+        this.completed = completed;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    public void setTargetDate(LocalDate targetDate) {
+        this.targetDate = targetDate;
+    }
+    public void setCompleted(boolean completed) {
+        this.completed = completed;
+    }
+    public int getId() {
+        return id;
+    }
+    public String getUsername() {
+        return username;
+    }
+    public String getDescription() {
+        return description;
+    }
+    public LocalDate getTargetDate() {
+        return targetDate;
+    }
+    public boolean isCompleted() {
+        return completed;
+    }
+
+    public String toString() {
+        return "Todo [id=" + id + ", username=" + username + ", description=" + description + ", targetDate=" + targetDate + ", completed=" + completed + "]";
+    }
+}
---
+++ projects/first-web-app/src/main/java/com/ayush953/first_web_app/todo/TodoService.java
@@ -0,0 +1,14 @@
+package com.ayush953.first_web_app.todo;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public class TodoService {
+    static List<Todo> todos;
+
+    static {
+        todos.add(new Todo(1,"Ayush","Learn Apache Kafka", LocalDate.now().plusYears(1),false));
+        todos.add(new Todo(2,"Ayush","Learn Docker", LocalDate.now().plusYears(1),false));
+        todos.add(new Todo(3,"Ayush","Learn Spring Boot", LocalDate.now().plusYears(1),false));
+    }
+}
